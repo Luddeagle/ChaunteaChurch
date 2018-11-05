bd6cad338284300463a6146827a88db65d09ac53
MDS\sean.hua7281 <>
2018-11-05 18:06:15 +1300

    created door open. glow should work soon

---
+++ ChaunteaChurch/Assets/Scripts/Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public float m_rotateAmount;
+    float m_doorOpenTime;
+    Quaternion m_startRot, m_endRot;
+    float m_rotateSpeed = 90.0f;
+
+    private void Awake()
+    {
+        m_startRot = transform.rotation;
+        m_endRot = transform.rotation * Quaternion.Euler(new Vector3(0, m_rotateAmount, 0));
+    }
+
+    private void Update()
+    {
+        if (m_doorOpenTime > 0)
+        {
+            m_doorOpenTime -= Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, m_endRot, m_rotateAmount * Time.deltaTime);
+        }
+        else
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, m_startRot, m_rotateAmount * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("Player"))
+        {
+            m_doorOpenTime = 5.0f;
+        }
+    }
+}
---
+++ ChaunteaChurch/Assets/Scripts/GlowToPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowToPlayer : MonoBehaviour {
+
+    public Material m_materialToGlow;
+    public float m_lowEnd = 0.01f;
+    public float m_upperEnd = 2.5f;
+    public float m_disToGlow = 4.0f;
+    public float m_moveSpd = 1.5f;
+    Transform m_player;
+    Color baseColor;
+    float m_currentVal;
+
+    private void Awake()
+    {
+        m_player = GameObject.Find("Player").transform;
+        baseColor = m_materialToGlow.GetColor("_EmissionColor");
+        baseColor.a = 1;
+        m_currentVal = m_lowEnd;
+    }
+
+    private void Update()
+    {
+        float target = m_lowEnd;
+        if ((transform.position - m_player.position).sqrMagnitude < m_disToGlow * m_disToGlow)
+            target = m_upperEnd;
+
+        m_currentVal = Mathf.MoveTowards(m_currentVal, target, m_moveSpd * Time.deltaTime);
+
+        Color finalColor = baseColor * Mathf.LinearToGammaSpace(m_currentVal);
+        GetComponent<Renderer>().material.SetColor("_EmissionColor", finalColor);
+    }
+}
